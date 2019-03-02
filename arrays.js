d0ddd1d8ebdd5c2615c1e8ad3632e13a9da603a3
Mathieu Houdet <mathieu.houdet@gmail.com>
2019-03-02 23:40:22 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -3,7 +3,10 @@ var chocolateBars = [ snickers, hundred, grand, kitkat, skittles]
 var addElementToBeginningOfArray = function () {
   var array1 = [test1, test2, test3];
   array1.unshift("test 0");
-  return array1
+  return array1;
 }
 
-var
+var destructivelyAddElementToBeginningOfArray = function () {
+  var array2 = ["testa",...addElementToBeginningOfArray];
+  return array2;
+}
