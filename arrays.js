57e4e904761922f2f5c46c1bf38c7e0b64f43ec3
Mathieu Houdet <mathieu.houdet@gmail.com>
2019-03-02 23:35:54 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,8 @@
 var chocolateBars = [ snickers, hundred, grand, kitkat, skittles]
+
+var addElementToBeginningOfArray = function () {
+var array1 = [test1, test2, test3];
+array1.unshift("test 0");
+
+
+}
