f5392ccfec5c4262fbfd512866c3f1a5025a5762
Mathieu Houdet <mathieu.houdet@gmail.com>
2019-03-02 23:49:08 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -7,8 +7,10 @@ var addElementToBeginningOfArray = function () {
 }
 console.log(addElementToBeginningOfArray);
 
+/*
 var destructivelyAddElementToBeginningOfArray = function () {
   var array2 = ["testa",...addElementToBeginningOfArray];
   return array2;
 }
 console.log(destructivelyAddElementToBeginningOfArray);
+*/
