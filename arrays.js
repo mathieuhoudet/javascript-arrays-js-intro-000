65760c5c84659522e2308c5174d8111dad8ae6b1
Mathieu Houdet <mathieu.houdet@gmail.com>
2019-03-03 00:02:13 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -4,12 +4,3 @@ console.log(chocolateBars);
 function addElementToBeginningOfArray(array1,newValue) {
   return [newValue,...array1];
 }
-addElementToBeginningOfArray(array1,"haha")
-
-/*
-var destructivelyAddElementToBeginningOfArray = function () {
-  var array2 = ["testa",...addElementToBeginningOfArray];
-  return array2;
-}
-console.log(destructivelyAddElementToBeginningOfArray);
-*/
