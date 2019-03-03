79417794321d17199cac2fa25610d9956570107d
Mathieu Houdet <mathieu.houdet@gmail.com>
2019-03-03 00:04:11 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -4,3 +4,8 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 function addElementToBeginningOfArray(array1,newValue) {
   return [newValue,...array1];
 }
+
+function destructivelyAddElementToBeginningOfArray(array1,newValue2){
+  var array1.unshift("newValue2");
+  return array1;
+}
